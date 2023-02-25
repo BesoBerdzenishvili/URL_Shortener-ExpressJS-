dcdd3e3d4fde6d7e6a00b44b2b5556d61f4fcda5
fliderman01 <fliderman01@gmail.com>
2023-02-25 12:07:25 +0400

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const urlRoutes = require("./routes/urlRoutes");
 const errorMiddleware = require("./middleware/errorMiddleware");
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // Middleware
